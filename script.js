ec5a4c21a91a7dd8d52866780004856361a17b3c
samimamansari <samimamansari@gmail.com>
2022-06-23 21:54:43 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,138 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
+
+let bookmarks = [];
+
+// Show Modal 
+function showModal(){
+    modal.classList.add("show-modal");
+    websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener("click",showModal);
+modalClose.addEventListener("click",()=>{
+    modal.classList.remove("show-modal");
+});
+window.addEventListener("click",(e)=> (e.target === modal ? modal.classList.remove("show-modal") : false));
+
+// Vaidate form
+function validate(nameValue,urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(!nameValue || !urlValue){
+        alert("Please enter values for both fields");
+        return false;
+    }
+    
+    if(!urlValue.match(regex)){
+        alert("Please enter valid Url!");
+        return false;
+    }
+    // valid
+    return true;
+}
+
+// Delete Bookmark
+function deleteBookmark(url){
+    bookmarks.forEach((bookmark,i)=>{
+        if(bookmark.url == url){
+            bookmarks.splice(i,1);
+        }
+    });
+    // Update bookmark  array in localstorage and repopulate dom
+    localStorage.setItem("bookmark",JSON.stringify(bookmarks));
+    fetchBookmarks();
+}
+
+// Build Bookmarks DOM
+function buildBookmarks(){
+    // Remove all bookmarks elements
+    bookmarksContainer.textContent = '';
+    // Build Items
+    bookmarks.forEach((bookmark)=>{
+        const {name, url} = bookmark;
+        // Item
+        const item = document.createElement("div");
+        item.classList.add("item");
+        // Close Icon
+        const closeIcon = document.createElement("i");
+        closeIcon.classList.add("fas","fa-times");
+        closeIcon.setAttribute("title","Delete Bookmark");
+        closeIcon.setAttribute("onclick",`deleteBookmark('${url}')`);
+        // Favicon Link Container
+        const linkInfo = document.createElement("div");
+        linkInfo.classList.add("name");
+        // Favicon
+        const favicon = document.createElement("img");
+        favicon.setAttribute("src",`https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute("alt","Favicon");
+        // Link
+        const link = document.createElement("a");
+        link.setAttribute("href",`${url}`);
+        link.setAttribute("target","_blank");
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon,link);
+        item.append(closeIcon,linkInfo);
+        bookmarksContainer.append(item);
+    });
+}
+
+//Fetch bookmark
+function fetchBookmarks(){
+    if(localStorage.getItem("bookmark"))
+    {
+        bookmarks = JSON.parse(localStorage.getItem("bookmark"));   
+
+    }
+    else
+    {
+       bookmarks = [
+            {
+                name : 'Jack',
+             url : "https://facebook.com",
+             }
+        ];
+      localStorage.setItem("bookmark",JSON.stringify(bookmarks));
+      console.log("emp"+bookmarks);
+
+    }
+    console.log(bookmarks);
+    buildBookmarks();
+} 
+
+//Handle form Data
+function storeBookmark(e){
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes("http://","https://"))
+    {
+        urlValue = `https://${urlValue}`;
+    }
+    if(!validate(nameValue,urlValue))
+    {
+        return false;
+    }
+    const bookmark = {
+        name: nameValue,
+        url:urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem("bookmark",JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
+}
+
+// Event Listener
+bookmarkForm.addEventListener("submit",storeBookmark);
+
+// Onload ,fetch all bookmarks
+fetchBookmarks();
